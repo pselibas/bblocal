f140aacf6ed6fcccaf318a6a87b5ff446bb66266
Paul Selibas <pselibas@gmail.com>
2022-01-17 20:59:14 +0100

    feature: converted from nodegit to simple-git for faster results

--- src/git.mjs
+++ src/git.mjs
@@ -1,23 +1,26 @@
-import Git from "nodegit";
 import temp from "temp";
+import simpleGit from "simple-git";
 
 // auto cleanup of created folders
-// let tempCleanup = temp.track();
-let tempCleanup = temp;
+let tempCleanup = temp.track();
+// let tempCleanup = temp;
 
 export async function clone(src) {
-    let currentRepo = await Git.Repository.open(".");
-    let branchName = (await currentRepo.getCurrentBranch()).shorthand();
-    let commitHash = (await currentRepo.getHeadCommit()).sha();
+
+    let currentRepo = simpleGit(".", { binary: "git" });
+
+    let branchName = await currentRepo.revparse(["--abbrev-ref", "HEAD"]);
+    let commitHash = await currentRepo.revparse(["HEAD"]);
 
     let destination = tempCleanup.mkdirSync("bb-local");
 
     console.log("Cloning '%s' to '%s'", src, destination);
-    let destinationRepo = await Git.Clone(src, destination);
+    let destinationRepo = simpleGit();
+    await destinationRepo.clone(src, destination);
 
     console.log("Checking out '%s'", commitHash);
-    await Git.Checkout.tree(destinationRepo, commitHash);
-
+    await destinationRepo.checkout(commitHash);
+    
     return {
         commitHash,
         branchName,
