17e79cb0ff00131d358af520363a7673224d6aea
Paul Selibas <pselibas@gmail.com>
2022-01-17 21:07:49 +0100

    feature: completed git replacement

--- src/git.mjs
+++ src/git.mjs
@@ -7,7 +7,7 @@ let tempCleanup = temp.track();
 
 export async function clone(src) {
 
-    let currentRepo = simpleGit(".", { binary: "git" });
+    let currentRepo = new simpleGit(src);
 
     let branchName = await currentRepo.revparse(["--abbrev-ref", "HEAD"]);
     let commitHash = await currentRepo.revparse(["HEAD"]);
@@ -15,12 +15,13 @@ export async function clone(src) {
     let destination = tempCleanup.mkdirSync("bb-local");
 
     console.log("Cloning '%s' to '%s'", src, destination);
-    let destinationRepo = simpleGit();
-    await destinationRepo.clone(src, destination);
+    let destinationRepoClone = new simpleGit({ baseDir: destination });
+    await destinationRepoClone.clone(src, destination);
 
     console.log("Checking out '%s'", commitHash);
+    let destinationRepo = new simpleGit(destination);
     await destinationRepo.checkout(commitHash);
-    
+
     return {
         commitHash,
         branchName,
